4ff52b7286c9e13d9d168d4131a2caf6f07fe9b4
artemm3708 <artemm37.08@gmail.com>
2022-12-18 13:55:33 +0200

    add search with parallel streams

--- test/src/main/java/com/eva/api/controller/ProductController.java
+++ test/src/main/java/com/eva/api/controller/ProductController.java
@@ -6,16 +6,24 @@ import com.eva.api.dto.request.ProductRequest;
 import com.eva.api.dto.response.ProductResponse;
 import com.eva.domain.Product;
 import com.eva.exception.NotFoundException;
-import com.eva.exception.NotValidFilter;
 import com.eva.service.ProductService;
+import com.google.gson.Gson;
 import jakarta.validation.Valid;
-import java.io.UnsupportedEncodingException;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,8 +32,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @Controller
+@Slf4j
 @RestController
 @RequestMapping("/shop")
 @RequiredArgsConstructor
@@ -33,6 +43,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
   ProductService productService;
+  Gson gson;
 
   @GetMapping("/products")
   public List<ProductResponse> getAll() {
@@ -56,17 +67,43 @@ public class ProductController {
 
   @GetMapping("/product")
   public List<ProductResponse> getAllByFilter(@Valid @RequestParam String filter) {
-    try {
       String regex = decode(filter);
-      return productService.findAll().stream()
-          .filter(product -> !(Pattern.matches(regex.toLowerCase(), product.getName().toLowerCase())))
-          .map(ProductResponse::fromEntity)
-          .toList();
-    } catch (UnsupportedEncodingException e) {
-      throw new NotValidFilter(filter);
-    }
+    log.info("request received to fetch all product details");
+    List<ProductResponse> products = productService.findAll().stream()
+        .filter(product -> !(Pattern.matches(regex.toLowerCase(), product.getName().toLowerCase())))
+        .map(ProductResponse::fromEntity)
+        .toList();
+    log.info("finished streaming records");
+    return products;
   }
-  private String decode(String value) throws UnsupportedEncodingException {
+
+  @GetMapping(value = "/product/streams")
+  public ResponseEntity<StreamingResponseBody> findAllProducts(@Valid @RequestParam String filter) {
+
+    String regex = decode(filter);
+    log.info("request received to fetch all product details");
+    Stream<ProductResponse> products = productService.findProductsByFilter()
+      .filter(product -> !(Pattern.matches(regex, product.getName())))
+      .map(ProductResponse::fromEntity);
+    StreamingResponseBody responseBody = httpResponseOutputStream -> {
+    try (Writer writer = new BufferedWriter(new OutputStreamWriter(httpResponseOutputStream))) {
+      products.forEach(product -> {
+        try {
+          writer.write(gson.toJson(product));
+          writer.flush();
+        } catch (IOException exception) {
+          log.error("exception occurred while writing object to stream", exception);
+        }
+      });
+    } catch (Exception exception) {
+      log.error("exception occurred while publishing data", exception);
+    }
+    log.info("finished streaming records");
+  };
+    return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(responseBody);
+}
+
+  private String decode(String value) {
     return URLDecoder.decode(value, StandardCharsets.UTF_8);
   }
 
--- test/src/main/java/com/eva/service/ProductService.java
+++ test/src/main/java/com/eva/service/ProductService.java
@@ -5,6 +5,7 @@ import com.eva.api.dto.request.ProductRequest;
 import com.eva.domain.Product;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface ProductService {
 
@@ -12,5 +13,6 @@ public interface ProductService {
 
   Optional<Product> findById(Long id);
   Product addProduct(ProductRequest request);
+  Stream<Product> findProductsByFilter();
 
 }
